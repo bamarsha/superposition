441ae9243488c1fbaea4891955fb80c8b2a55228
Sarah Marshall <sarah@sarahmarshall.name>
2020-04-17 00:57:40 -0700

    Clean up Laser

--- SuperpositionGame/src/main/scala/superposition/game/Laser.scala
+++ SuperpositionGame/src/main/scala/superposition/game/Laser.scala
@@ -3,13 +3,14 @@ package superposition.game
 import java.net.URL
 
 import engine.core.Behavior.Entity
+import engine.core.Game
 import engine.core.Game.{dt, track}
-import engine.core.{Game, Input}
+import engine.core.Input.{mouse, mouseJustPressed}
 import engine.graphics.Graphics._
 import engine.graphics.sprites.Sprite
 import engine.util.Color
 import engine.util.Color._
-import engine.util.math.{Transformation, Vec2d}
+import engine.util.math.Transformation
 import superposition.game.Laser._
 import superposition.math.Direction.{Down, Left, Right, Up}
 import superposition.math.{Direction, Vec2i}
@@ -17,27 +18,7 @@ import superposition.quantum.{Gate, MetaId, StateId, Universe}
 
 import scala.Function.const
 import scala.jdk.CollectionConverters._
-
-/**
- * Contains initialization for lasers.
- */
-private object Laser {
-  val All: Iterable[Laser] = track(classOf[Laser]).asScala
-
-  private val Sprites: Map[Direction, URL] = Map(
-    Up -> getClass.getResource("sprites/laser_up.png"),
-    Down -> getClass.getResource("sprites/laser_down.png"),
-    Left -> getClass.getResource("sprites/laser_left.png"),
-    Right -> getClass.getResource("sprites/laser_right.png")
-  )
-  private val BeamDuration: Double = 0.2
-  private val FadeDuration: Double = 0.3
-
-  /**
-   * Declares the laser system.
-   */
-  def declareSystem(): Unit = Game.declareSystem(classOf[Laser], (_: Laser).step())
-}
+import scala.math.min
 
 /**
  * A laser applies a quantum gate to any qubit hit by its beam.
@@ -52,56 +33,81 @@ private final class Laser(multiverse: Multiverse,
                           cell: Vec2i,
                           gate: Gate[StateId[Boolean]],
                           direction: Direction,
-                          controls: List[Vec2i]) extends Entity {
+                          controls: Iterable[Vec2i]) extends Entity {
+  private val targetCell: MetaId[Option[Vec2i]] = multiverse.createIdMeta(None)
 
-  // Metadata
-  val targetCell: MetaId[Option[Vec2i]] = multiverse.createIdMeta(None)
-  val elapsedTime: MetaId[Double] = multiverse.createIdMeta(0)
+  private val elapsedTime: MetaId[Double] = multiverse.createIdMeta(0)
 
-  val sprite: SpriteComponent = add(new SpriteComponent(this,
-    _ => Sprite.load(Laser.Sprites(direction)), _ => cell.toVec2d.add(.5)))
+  add(new SpriteComponent(this,
+    sprite = const(Sprite.load(Sprites(direction))),
+    position = const(cell.toVec2d add 0.5)))
 
-  val universe: UniverseComponent = add(new UniverseComponent(this, multiverse))
-  universe.blockingCells = _ => List(cell)
+  private val universe: UniverseComponent = add(new UniverseComponent(this, multiverse))
+  universe.blockingCells = const(List(cell))
 
-  def draw(u: Universe): Unit = {
-    if (selected()) {
+  private val beam: LazyList[Vec2i] = LazyList.iterate(cell)(_ + direction.toVec2i).tail
+
+  /**
+   * Draws the laser in a universe.
+   *
+   * @param universe the universe to draw in
+   */
+  def draw(universe: Universe): Unit = {
+    if (selected) {
       drawRectangleOutline(Transformation.create(cell.toVec2d, 0, 1), RED)
     }
-    val tc = u.meta(targetCell)
-    val et = u.meta(elapsedTime)
-    if (tc.isDefined && et <= BeamDuration + FadeDuration) {
-      drawWideLine(
-        cell.toVec2d.add(.5),
-        new Vec2d(tc.get.x + 0.5, tc.get.y + 0.5),
-        0.25,
-        new Color(1, 0, 0, math.min(FadeDuration, BeamDuration + FadeDuration - et) / FadeDuration)
-      )
+    universe.meta(targetCell) match {
+      case Some(targetCell) if universe.meta(elapsedTime) <= BeamDuration + FadeDuration =>
+        drawWideLine(
+          cell.toVec2d add 0.5,
+          targetCell.toVec2d add 0.5,
+          0.25,
+          new Color(1, 0, 0,
+            min(FadeDuration, BeamDuration + FadeDuration - universe.meta(elapsedTime)) / FadeDuration))
+      case _ => ()
     }
   }
 
-  private def beam: LazyList[Vec2i] = LazyList.iterate(cell)(_ + direction.toVec2i).tail
-
-  private def targetCell(u: Universe): Option[Vec2i] =
-    if (u.allOn(controls))
-      beam.take(50) find (cell => u.isBlocked(cell) || u.allInCell(cell).nonEmpty)
+  private def targetCell(universe: Universe): Option[Vec2i] =
+    if (universe.allOn(controls))
+      beam.take(50) find (cell => universe.isBlocked(cell) || universe.allInCell(cell).nonEmpty)
     else
       None
 
-  private def hits(u: Universe): List[StateId[Boolean]] = targetCell(u).toList.flatMap(u.getPrimaryBits)
+  private def hits(universe: Universe): List[StateId[Boolean]] =
+    targetCell(universe).toList flatMap universe.getPrimaryBits
 
-  private val actualGate = gate.multi control const(hits)
-
-  private def selected(): Boolean = Vec2i(Input.mouse().x.floor.toInt, Input.mouse().y.floor.toInt) == cell
+  private def selected: Boolean =
+    cell == Vec2i(mouse().x.floor.toInt, mouse().y.floor.toInt)
 
   private def step(): Unit = {
-    if (Input.mouseJustPressed(0) && selected()) {
-      multiverse.applyGate(actualGate, ())
-      multiverse.universes = multiverse.universes map (u => u.updatedMeta(targetCell)(targetCell(u)))
-      multiverse.universes = multiverse.universes.map {
-        u => if (targetCell(u).isEmpty) u else u.updatedMeta(elapsedTime)(0)
+    if (mouseJustPressed(0) && selected) {
+      multiverse.applyGate(gate.multi control const(hits), ())
+      multiverse.universes = multiverse.universes map { universe =>
+        if (targetCell(universe).isEmpty)
+          universe.updatedMeta(targetCell)(None)
+        else
+          universe
+            .updatedMeta(targetCell)(targetCell(universe))
+            .updatedMeta(elapsedTime)(0)
       }
     }
     multiverse.universes = multiverse.universes map (_.updatedMetaWith(elapsedTime)(_ + dt))
   }
+}
+
+private object Laser {
+  val All: Iterable[Laser] = track(classOf[Laser]).asScala
+
+  private val Sprites: Map[Direction, URL] = Map(
+    Up -> getClass.getResource("sprites/laser_up.png"),
+    Down -> getClass.getResource("sprites/laser_down.png"),
+    Left -> getClass.getResource("sprites/laser_left.png"),
+    Right -> getClass.getResource("sprites/laser_right.png"))
+
+  private val BeamDuration: Double = 0.2
+
+  private val FadeDuration: Double = 0.3
+
+  def declareSystem(): Unit = Game.declareSystem(classOf[Laser], (_: Laser).step())
 }
