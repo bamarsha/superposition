3c8b314df8a294519442a67a86f5200b0a8346b7
Sarah Marshall <sarah@sarahmarshall.name>
2020-04-17 01:58:55 -0700

    Clean up Level.scala

--- SuperpositionGame/src/main/scala/superposition/game/Level.scala
+++ SuperpositionGame/src/main/scala/superposition/game/Level.scala
@@ -1,21 +1,23 @@
 package superposition.game
 
 import engine.core.Behavior.Entity
-import engine.core.{Game, Input}
+import engine.core.Game
+import engine.core.Input.keyJustPressed
 import engine.graphics.Camera
 import extras.tiles.Tilemap
 import org.lwjgl.glfw.GLFW.GLFW_KEY_R
 import superposition.math.{Direction, Vec2i}
-import superposition.quantum.{H, Universe, X}
+import superposition.quantum._
 
 import scala.collection.immutable.HashMap
 import scala.jdk.CollectionConverters._
+import scala.sys.error
 
 /**
  * Manages game levels.
  */
 private object Level {
-  private var supplier: () => Option[Multiverse] = () => None
+  private var createMultiverse: () => Option[Multiverse] = () => None
 
   private var multiverse: Option[Multiverse] = None
 
@@ -23,118 +25,109 @@ private object Level {
    * Declares the level system.
    */
   def declareSystem(): Unit =
-    Game.declareSystem(() =>
-      if (Input.keyJustPressed(GLFW_KEY_R)) {
-        multiverse foreach Game.destroy
-        multiverse = supplier()
-        multiverse foreach Game.create
+    Game.declareSystem { () =>
+      if (keyJustPressed(GLFW_KEY_R)) {
+        multiverse.foreach(Game.destroy)
+        multiverse = createMultiverse()
+        multiverse.foreach(Game.create)
       }
-    )
+    }
 
   /**
    * Loads the multiverse as a new level.
    *
-   * @param multiverse an expression that creates a new multiverse to load
+   * @param multiverse the multiverse
    */
   def load(multiverse: => Multiverse): Unit = {
-    this.multiverse foreach Game.destroy
-    supplier = () => Some(multiverse)
+    this.multiverse.foreach(Game.destroy)
 
     val current = multiverse
     Camera.camera2d.lowerLeft = current.boundingBox.lowerLeft
     Camera.camera2d.upperRight = current.boundingBox.upperRight
     Game.create(current)
 
+    createMultiverse = () => Some(multiverse)
     this.multiverse = Some(current)
   }
 
   /**
-   * Loads the tilemap as a new level.
+   * Loads the tile map as a new level.
    *
-   * @param tilemap the tilemap to load
+   * @param tileMap the tile map to load
    */
-  def load(tilemap: Tilemap): Unit = {
+  def load(tileMap: Tilemap): Unit =
     load {
-      val universe = Universe(walls = wallsInTilemap(tilemap))
-      val multiverse = new Multiverse(universe, tilemap)
-      var entityMap = new HashMap[Int, Entity]()
+      val universe = Universe(walls = wallsInTileMap(tileMap))
+      val multiverse = new Multiverse(universe, tileMap)
+      var entities = new HashMap[Int, Entity]
 
-      for ((group, layer) <- tilemap.objectGroups.asScala.zipWithIndex;
+      for ((group, layer) <- tileMap.objectGroups.asScala.zipWithIndex;
            obj <- group.objects.asScala) {
-        val (entity, id) = entityFromObject(tilemap, obj, multiverse)
-        if (entity != null) {
-          Game.create(entity)
-          multiverse.entities ::= entity
-          entityMap += id -> entity
-        }
-//        entity.layer = layer
-//         universe.add(entity)
+        val entity = entityFromObject(tileMap, obj, multiverse)
+        println("Spawning " + obj.`type` + ".")
+        Game.create(entity)
+        multiverse.entities ::= entity
+        entities += obj.id -> entity
+        // TODO: entity.layer = layer
       }
 
-      val gates = tilemap.properties.asScala.get("Gates")
-      for (Array(gate, target) <- gates.iterator.flatMap(_.value.linesIterator).map(_.split(' '))) {
-        println("Applying gate " + gate + " on " + target)
-        // multiverse.applyGate(gate, target)
-        val targetBit = entityMap.get(target.toInt).map(_.get(classOf[UniverseComponent])).flatMap(_.primaryBit)
-        gate match {
-          case "X" => multiverse.applyGate(X, targetBit.get)
-          case "H" => multiverse.applyGate(H, targetBit.get)
-        }
+      val gates = tileMap.properties.asScala.get("Gates")
+      for (Array(gateName, targetId) <- gates.iterator flatMap (_.value.linesIterator) map (_.split(' '))) {
+        println("Applying gate " + gateName + " on entity " + targetId + ".")
+        val target = entities(targetId.toInt).get(classOf[UniverseComponent]).primaryBit.get
+        multiverse.applyGate(gateFromName(gateName), target)
       }
 
       multiverse
     }
-  }
 
-  private def entityFromObject(tilemap: Tilemap,
-                               obj: Tilemap#ObjectGroup#Object,
-                               multiverse: Multiverse): (Entity, Int) = {
-     val id = obj.id
-//    val cell = Cell(
-//      tilemap.height - (obj.y / tilemap.tileHeight).floor.toInt - 1,
-//      (obj.x / tilemap.tileWidth).floor.toInt
-//    )
+  private def entityFromObject(tileMap: Tilemap, obj: Tilemap#ObjectGroup#Object, multiverse: Multiverse): Entity = {
     val cell = Vec2i(
-      (obj.x / tilemap.tileWidth).floor.toInt,
-      tilemap.height - (obj.y / tilemap.tileHeight).floor.toInt - 1)
+      (obj.x / tileMap.tileWidth).floor.toInt,
+      tileMap.height - (obj.y / tileMap.tileHeight).floor.toInt - 1)
     val properties = obj.properties.asScala
-    println("Spawning " + obj.`type`)
-    (obj.`type` match {
+    obj.`type` match {
       case "Player" => new Player(multiverse, cell)
       case "Quball" => new Quball(multiverse, cell)
       case "Laser" =>
-        val gate = X // Gate.withName(properties("Gate").value)
+        val gate = gateFromName(properties("Gate").value)
         val direction = Direction.withName(properties("Direction").value)
-        val control = properties.get("Control").flatMap(c => cellFromString(tilemap, c.value)).toList
+        val control = (properties.get("Control") map (control => cellFromString(tileMap, control.value))).toList
         new Laser(multiverse, cell, gate, direction, control)
       case "Door" =>
-        val controls = cellsFromString(tilemap, properties("Controls").value).toList
+        val controls = cellsFromString(tileMap, properties("Controls").value).toList
         new Door(multiverse, cell, controls)
       case "Goal" =>
-        // val requires = ObjectId(properties("Requires").value.toInt)
-        lazy val requires = Player.All.head.cell
+        // TODO: val requires = ObjectId(properties("Requires").value.toInt)
         val next = properties("Next Level").value
-        new Goal(multiverse, cell, requires, () => load(Tilemap.load(getClass.getResource(next))))
-      case _ => null
-    }, id)
+        new Goal(multiverse, cell,
+          required = Player.All.head.cell,
+          action = () => load(Tilemap.load(getClass.getResource(next))))
+      case _ => error("Unknown entity type '" + obj.`type` + "'.")
+    }
   }
 
-  private def cellFromString(tilemap: Tilemap, string: String): Option[Vec2i] =
-    """\((\d+),\s*(\d+)\)"""
-      .r("column", "row")
-      .findFirstMatchIn(string)
-      .map(m => Vec2i(m.group("column").trim.toInt, tilemap.height - m.group("row").trim.toInt - 1))
+  private def cellFromString(tileMap: Tilemap, string: String): Vec2i =
+    """\((\d+),\s*(\d+)\)""".r("x", "y").findFirstMatchIn(string) match {
+      case Some(m) => Vec2i(m.group("x").trim.toInt, tileMap.height - m.group("y").trim.toInt - 1)
+      case None => error("Invalid cell '" + string + "'.")
+    }
 
-  private def cellsFromString(tilemap: Tilemap, string: String): Seq[Vec2i] =
-    string.linesIterator.flatMap(cellFromString(tilemap, _)).toSeq
+  private def cellsFromString(tileMap: Tilemap, string: String): Seq[Vec2i] =
+    (string.linesIterator map (cellFromString(tileMap, _))).toSeq
 
-  private def wallsInTilemap(tilemap: Tilemap): Set[Vec2i] =
-    (for (layer <- tilemap.layers.asScala if layer.properties.asScala.get("Collision").exists(_.value.toBoolean);
+  private def gateFromName(name: String): Gate[StateId[Boolean]] = name match {
+    case "X" => X
+    case "H" => H
+    case _ => error("Unsupported gate '" + name + "'.")
+  }
+
+  private def wallsInTileMap(tileMap: Tilemap): Set[Vec2i] =
+    (for (layer <- tileMap.layers.asScala if layer.properties.asScala.get("Collision") exists (_.value.toBoolean);
           x <- 0 until layer.width;
           y <- 0 until layer.height if layer.data.tiles(x)(y) != 0) yield {
       Vec2i(
-        (x + layer.offsetX.toDouble / tilemap.tileWidth).round.toInt,
-        (y + layer.offsetY.toDouble / tilemap.tileHeight).round.toInt
-      )
+        (x + layer.offsetX.toDouble / tileMap.tileWidth).round.toInt,
+        (y + layer.offsetY.toDouble / tileMap.tileHeight).round.toInt)
     }).toSet
 }
