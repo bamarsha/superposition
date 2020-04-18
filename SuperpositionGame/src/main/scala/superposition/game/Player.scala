55dd76370d9e8f3e080437c95d5afedc90a1be2f
Sarah Marshall <sarah@sarahmarshall.name>
2020-04-18 04:08:18 -0700

    Clean up Player

--- SuperpositionGame/src/main/scala/superposition/game/Player.scala
+++ SuperpositionGame/src/main/scala/superposition/game/Player.scala
@@ -1,37 +1,19 @@
 package superposition.game
 
 import engine.core.Behavior.Entity
+import engine.core.Game
 import engine.core.Game.{dt, track}
-import engine.core.{Game, Input}
+import engine.core.Input.{keyDown, keyJustPressed}
 import engine.graphics.sprites.Sprite
 import engine.util.Color._
 import engine.util.math.Vec2d
 import org.lwjgl.glfw.GLFW._
+import superposition.game.Player.{CatSprite, deltaPosition, snapPosition}
 import superposition.math.Vec2i
 import superposition.quantum._
 
 import scala.Function.const
 import scala.jdk.CollectionConverters._
-
-/**
- * Contains settings and initialization for the player.
- */
-object Player {
-  private val WalkKeys: Map[Int, Vec2d] = Map(
-    GLFW_KEY_W -> new Vec2d(0, 1),
-    GLFW_KEY_A -> new Vec2d(-1, 0),
-    GLFW_KEY_S -> new Vec2d(0, -1),
-    GLFW_KEY_D -> new Vec2d(1, 0)
-  )
-
-  /**
-   * Declares the player system.
-   */
-  def declareSystem(): Unit = Game.declareSystem(classOf[Player], (_: Player).step())
-
-  val All: Iterable[Player] = track(classOf[Player]).asScala
-  private val CatSprite = Sprite.load(getClass.getResource("sprites/cat.png"))
-}
 
 /**
  * The player character in the game.
@@ -40,92 +22,111 @@ object Player {
  * @param initialCell the initial position for this player
  */
 class Player(multiverse: Multiverse, initialCell: Vec2i) extends Entity {
+  private val alive: StateId[Boolean] = multiverse.allocate(true)
 
-  // Quantum state
-  val alive: StateId[Boolean] = multiverse.allocate(true)
   val cell: StateId[Vec2i] = multiverse.allocate(initialCell)
 
-  // Metadata
-  val position: MetaId[Vec2d] = multiverse.allocateMeta(initialCell.toVec2d.add(.5))
+  private val position: MetaId[Vec2d] = multiverse.allocateMeta(initialCell.toVec2d add 0.5)
 
-  val sprite: SpriteComponent = add(new SpriteComponent(this,
-    _ => Player.CatSprite, _.meta(position), _ => new Vec2d(2, 2), u => if (u.state(alive)) WHITE else BLACK))
+  private var relativePosition = new Vec2d(0.5, 0.5)
 
-  val universe: UniverseComponent = add(new UniverseComponent(this, multiverse))
+  add(new SpriteComponent(this,
+    sprite = const(CatSprite),
+    position = _.meta(position),
+    scale = const(new Vec2d(2, 2)),
+    universe => if (universe.state(alive)) WHITE else BLACK))
+
+  private val universe: UniverseComponent = add(new UniverseComponent(this, multiverse))
   universe.primaryBit = Some(alive)
   universe.position = Some(cell)
 
-  var cellPosition = new Vec2d(0.5, 0.5)
-
-  private def step(): Unit = {
-    cellPosition = cellPositionFromInput
-    val (x, y) = walkGates()
-    if (walk(x, 0)) cellPosition = cellPosition.sub(new Vec2d(x, 0))
-    if (walk(0, y)) cellPosition = cellPosition.sub(new Vec2d(0, y))
-    cellPosition = cellPosition.clamp(0, 1)
-
-    if (Input.keyJustPressed(GLFW_KEY_SPACE)) {
-      toggleCarrying()
+  private val walkGate: Gate[Vec2i] = {
+    val movePlayer: Gate[Vec2i] = Translate.multi control { delta => universe =>
+      if (universe.state(alive)) List((cell, delta)) else List()
     }
+    val moveQuballs: Gate[Vec2i] = Translate.multi control { delta => universe =>
+      if (universe.state(alive))
+        Quball.All
+          .filter(quball => universe.state(quball.carried))
+          .map(quball => (quball.cell, delta))
+          .toList
+      else List()
+    }
+    movePlayer andThen moveQuballs
+  }
 
+  private val carryGate: Gate[Unit] = X.multi control const { universe =>
+    Quball.All
+      .filter(quball => universe.state(alive) && universe.state(cell) == universe.state(quball.cell))
+      .map(_.carried)
+      .toList
+  }
+
+  private def walkIntent(): Vec2i = {
+    val next = relativePosition add deltaPosition()
+    Vec2i(snapPosition(next.x), snapPosition(next.y))
+  }
+
+  private def walk(): Unit = {
+    def applyGate(delta: Vec2i) = multiverse.applyGate(walkGate, delta)
+
+    val Vec2i(dx, dy) = walkIntent()
+    val delta = deltaPosition() add new Vec2d(
+      if (dx != 0 && applyGate(Vec2i(dx, 0))) -dx else 0,
+      if (dy != 0 && applyGate(Vec2i(0, dy))) -dy else 0)
+    relativePosition = (relativePosition add delta).clamp(0, 1)
+  }
+
+  private def updatePosition(): Unit = {
     multiverse.updateMetaWith(position) { pos => universe =>
       if (universe.state(alive)) {
-        val targetPosition = universe.state(cell).toVec2d add cellPosition
+        val targetPosition = universe.state(cell).toVec2d add relativePosition
         pos.lerp(targetPosition, 10 * dt)
       } else pos
     }
 
     for (quball <- Quball.All) {
       multiverse.updateMetaWith(quball.position) { pos => universe =>
-        val relativePos = if (universe.state(quball.carried)) cellPosition else new Vec2d(0.5, 0.5)
+        val relativePos = if (universe.state(quball.carried)) relativePosition else new Vec2d(0.5, 0.5)
         val targetPos = universe.state(quball.cell).toVec2d add relativePos
         if (universe.state(alive)) pos.lerp(targetPos, 10 * dt) else pos
       }
     }
   }
 
-  private def cellPositionFromInput: Vec2d = {
-    val delta = Player.WalkKeys.foldLeft(new Vec2d(0, 0)) {
-      case (delta, (key, direction)) => if (Input.keyDown(key)) delta add direction else delta
+  private def step(): Unit = {
+    walk()
+    if (keyJustPressed(GLFW_KEY_SPACE)) {
+      multiverse.applyGate(carryGate, ())
     }
-    cellPosition add (if (delta.length == 0) new Vec2d(0, 0) else delta.setLength(6.5 * dt))
+    updatePosition()
+  }
+}
+
+object Player {
+  val All: Iterable[Player] = track(classOf[Player]).asScala
+
+  private val WalkKeys: Map[Int, Vec2d] = Map(
+    GLFW_KEY_W -> new Vec2d(0, 1),
+    GLFW_KEY_A -> new Vec2d(-1, 0),
+    GLFW_KEY_S -> new Vec2d(0, -1),
+    GLFW_KEY_D -> new Vec2d(1, 0))
+
+  private val CatSprite: Sprite = Sprite.load(getClass.getResource("sprites/cat.png"))
+
+  private val Speed: Double = 6.5
+
+  def declareSystem(): Unit = Game.declareSystem(classOf[Player], (_: Player).step())
+
+  private def deltaPosition(): Vec2d = {
+    val delta = WalkKeys.foldLeft(new Vec2d(0, 0)) {
+      case (delta, (key, direction)) => if (keyDown(key)) delta add direction else delta
+    }
+    if (delta.length == 0) delta else delta.setLength(Speed * dt)
   }
 
-  private def walkGates(): (Int, Int) = {
-    var x = 0
-    var y = 0
-    if (cellPosition.x < -1e-3) x -= 1
-    if (cellPosition.x > 1 + 1e-3) x += 1
-    if (cellPosition.y < -1e-3) y -= 1
-    if (cellPosition.y > 1 + 1e-3) y += 1
-    (x, y)
-  }
-
-  private def walk(x: Int, y: Int): Boolean =
-    if (x == 0 && y == 0) {
-      true
-    } else {
-      val translatePlayer: Gate[Vec2i] = Translate.multi control {
-        delta => universe => if (universe.state(alive)) List((cell, delta)) else List()
-      }
-      val translateQuballs: Gate[Vec2i] = Translate.multi control {
-        delta => universe =>
-          if (universe.state(alive))
-            (Quball.All
-              filter (quball => universe.state(quball.carried))
-              map (quball => (quball.cell, delta)))
-              .toList
-          else List()
-      }
-      multiverse.applyGate(translatePlayer andThen translateQuballs, Vec2i(x, y))
-    }
-
-  private def toggleCarrying(): Boolean =
-    multiverse.applyGate[Unit](X.multi control const {
-      universe =>
-        (Quball.All
-          filter (quball => universe.state(alive) && universe.state(cell) == universe.state(quball.cell))
-          map (_.carried))
-          .toList
-    }, ())
+  private def snapPosition(delta: Double): Int =
+    if (delta < -1e-3) -1
+    else if (delta > 1 + 1e-3) 1
+    else 0
 }
