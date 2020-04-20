331ed4862f5c01b5df8d9cd749fbee8ab21be3d5
Sarah Marshall <sarah@sarahmarshall.name>
2020-04-19 23:01:25 -0700

    Make DependentMap a case class

--- SuperpositionGame/src/main/scala/superposition/math/dependentMap.scala
+++ SuperpositionGame/src/main/scala/superposition/math/dependentMap.scala
@@ -2,14 +2,14 @@ package superposition.math
 
 import scala.collection.immutable.HashMap
 
-final class DependentMap[K <: DependentKey] private(map: HashMap[K, Any]) {
+final case class DependentMap[K <: DependentKey] private(map: HashMap[K, Any]) extends AnyVal {
+  def apply(key: K): key.Value = get(key).get
+
   def get(key: K): Option[key.Value] = map.get(key).asInstanceOf[Option[key.Value]]
 
-  def updated(key: K)(value: key.Value): DependentMap[K] = new DependentMap(map.updated(key, value))
+  def updated(key: K)(value: key.Value): DependentMap[K] = DependentMap(map.updated(key, value))
 
-  def removed(key: K): DependentMap[K] = new DependentMap(map.removed(key))
-
-  def apply(key: K): key.Value = get(key).get
+  def removed(key: K): DependentMap[K] = DependentMap(map.removed(key))
 
   def updatedWith(key: K)(updater: Option[key.Value] => Option[key.Value]): DependentMap[K] =
     (get(key), updater(get(key))) match {
@@ -20,7 +20,7 @@ final class DependentMap[K <: DependentKey] private(map: HashMap[K, Any]) {
 }
 
 object DependentMap {
-  def empty[K <: DependentKey]: DependentMap[K] = new DependentMap(new HashMap)
+  def empty[K <: DependentKey]: DependentMap[K] = DependentMap(new HashMap)
 }
 
 trait DependentKey {
