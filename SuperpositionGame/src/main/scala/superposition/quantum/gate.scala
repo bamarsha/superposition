9f0125d5d568e1dfadbae9653a9025fe174c49ee
Sarah Marshall <sarah@sarahmarshall.name>
2020-04-21 23:53:15 -0700

    Gate.apply can return NonEmptyList[Universe]

--- SuperpositionGame/src/main/scala/superposition/quantum/gate.scala
+++ SuperpositionGame/src/main/scala/superposition/quantum/gate.scala
@@ -1,6 +1,6 @@
 package superposition.quantum
 
-import scalaz.Divisible
+import scalaz.{Divisible, NonEmptyList}
 import superposition.math.{Complex, Vec2i}
 
 import scala.math.sqrt
@@ -18,7 +18,7 @@ sealed trait Gate[A] {
    * @param universe The universe in which to apply the value.
    * @return The universes produced by the gate.
    */
-  def apply(value: A)(universe: Universe): Iterable[Universe]
+  def apply(value: A)(universe: Universe): NonEmptyList[Universe]
 
   /**
    * The adjoint is the reverse of the gate.
@@ -32,7 +32,7 @@ object Gate {
     override def conquer[A]: Gate[A] = Identity
 
     override def divide2[A, B, C](gate1: => Gate[A], gate2: => Gate[B])(f: C => (A, B)): Gate[C] = new Gate[C] {
-      override def apply(value: C)(universe: Universe): Iterable[Universe] = {
+      override def apply(value: C)(universe: Universe): NonEmptyList[Universe] = {
         val (a, b) = f(value)
         gate1(a)(universe) flatMap gate2(b)
       }
@@ -46,14 +46,14 @@ object Gate {
     import Gate.GateDivisible.divisibleSyntax._
 
     def applyToAll(value: A)(universes: Iterable[Universe]): Iterable[Universe] =
-      universes flatMap gate(value)
+      universes flatMap (gate(value)(_).stream)
 
     def andThen(other: Gate[A]): Gate[A] =
       Divisible[Gate].divide(gate, other)(value => (value, value))
 
     def multi: Gate[Seq[A]] = new Gate[Seq[A]] {
-      override def apply(values: Seq[A])(universe: Universe): Iterable[Universe] = values match {
-        case Seq() => Iterable(universe)
+      override def apply(values: Seq[A])(universe: Universe): NonEmptyList[Universe] = values match {
+        case Seq() => NonEmptyList(universe)
         case x :: xs => gate(x)(universe) flatMap gate.multi(xs)
       }
 
@@ -61,7 +61,7 @@ object Gate {
     }
 
     def controlled[B](f: B => Universe => A): Gate[B] = new Gate[B] {
-      override def apply(value: B)(universe: Universe): Iterable[Universe] = {
+      override def apply(value: B)(universe: Universe): NonEmptyList[Universe] = {
         val result = gate(f(value)(universe))(universe)
         for (newUniverse <- result) {
           assert(f(value)(universe) == f(value)(newUniverse))
@@ -83,7 +83,7 @@ case object Identity extends Gate[Any] {
 
   import scala.language.implicitConversions
 
-  override def apply(value: Any)(universe: Universe) = List(universe)
+  override def apply(value: Any)(universe: Universe): NonEmptyList[Universe] = NonEmptyList(universe)
 
   override def adjoint: Gate[Any] = this
 
@@ -91,17 +91,16 @@ case object Identity extends Gate[Any] {
 }
 
 case object X extends Gate[StateId[Boolean]] {
-  override def apply(id: StateId[Boolean])(universe: Universe): Iterable[Universe] =
-    List(universe.updatedStateWith(id)(!_))
+  override def apply(id: StateId[Boolean])(universe: Universe): NonEmptyList[Universe] =
+    NonEmptyList(universe.updatedStateWith(id)(!_))
 
   override def adjoint: Gate[StateId[Boolean]] = this
 }
 
 case object H extends Gate[StateId[Boolean]] {
-  override def apply(id: StateId[Boolean])(universe: Universe): Iterable[Universe] = List(
+  override def apply(id: StateId[Boolean])(universe: Universe): NonEmptyList[Universe] = NonEmptyList(
     universe / Complex((if (universe.state(id)) -1 else 1) * sqrt(2)),
-    universe.updatedStateWith(id)(!_) / Complex(sqrt(2))
-  )
+    universe.updatedStateWith(id)(!_) / Complex(sqrt(2)))
 
   override def adjoint: Gate[StateId[Boolean]] = this
 }
@@ -110,8 +109,8 @@ case object Translate extends Gate[(StateId[Vec2i], Vec2i)] {
 
   import Gate.GateDivisible.divisibleSyntax._
 
-  override def apply(value: (StateId[Vec2i], Vec2i))(universe: Universe): Iterable[Universe] = value match {
-    case (id, delta) => List(universe.updatedStateWith(id)(_ + delta))
+  override def apply(value: (StateId[Vec2i], Vec2i))(universe: Universe): NonEmptyList[Universe] = value match {
+    case (id, delta) => NonEmptyList(universe.updatedStateWith(id)(_ + delta))
   }
 
   override def adjoint: Gate[(StateId[Vec2i], Vec2i)] =
