9887206259a04e731c58ef53bf7410f786b5e43d
Sarah Marshall <sarah@sarahmarshall.name>
2020-05-05 22:15:54 -0700

    Add doc comments to Activator

--- src/superposition/game/component/Activator.scala
+++ src/superposition/game/component/Activator.scala
@@ -3,8 +3,14 @@ package superposition.game.component
 import com.badlogic.ashley.core.{Component, ComponentMapper}
 import superposition.quantum.StateId
 
+/** The activator component can activate objects that require a qubit in a particular state.
+  *
+  * @param activator the activator qubit
+  */
 final class Activator(val activator: StateId[Boolean]) extends Component
 
+/** Contains the component mapper for the activator component. */
 object Activator {
+  /** The component mapper for the activator component. */
   val Mapper: ComponentMapper[Activator] = ComponentMapper.getFor(classOf[Activator])
 }
