fef417b1071b9eccd08c6b3608b0a092f9881348
Sarah Marshall <sarah@sarahmarshall.name>
2020-05-16 00:37:53 -0700

    Make CellHighlighter an object

--- src/superposition/game/component/CellHighlighter.scala
+++ src/superposition/game/component/CellHighlighter.scala
@@ -5,10 +5,7 @@ import com.badlogic.ashley.core.{Component, Entity}
 import scala.Function.const
 
 /** A tag for the entity responsible for highlighting occupied cells in the multiverse. */
-final class CellHighlighter extends Component
-
-/** Contains the factory for the cell highlighter. */
-object CellHighlighter {
+object CellHighlighter extends Component {
   /** Makes a renderable cell highlighter entity.
     *
     * @param layer the layer to render the cell highlights in
@@ -16,5 +13,5 @@ object CellHighlighter {
     */
   def makeEntity(layer: Int): Entity = (new Entity)
     .add(new Renderable(layer, const(())))
-    .add(new CellHighlighter)
+    .add(CellHighlighter)
 }
