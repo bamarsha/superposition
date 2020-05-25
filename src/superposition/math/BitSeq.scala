2fc7a80a934a701ceab908b5e7be0590e6c2b3fb
Sarah Marshall <sarah@sarahmarshall.name>
2020-05-25 05:01:49 -0700

    Replace BitSet with Int

--- src/superposition/math/BitSeq.scala
+++ src/superposition/math/BitSeq.scala
@@ -1,23 +1,20 @@
 package superposition.math
 
-import scala.collection.immutable.BitSet
-import scala.sys.error
-
 /** A sequence of bits.
   *
   * @param bits the bits in the sequence
   * @param length the length of the sequence
   */
-final class BitSeq private(private val bits: BitSet, val length: Int) {
+final class BitSeq private(private val bits: Int, val length: Int) {
   /** Returns true if the `i`th bit is 1.
     *
     * @param i the index
     * @return true if the `i`th bit is 1
     */
-  def apply(i: Int): Boolean = bits(i)
+  def apply(i: Int): Boolean = ((bits >> i) & 1) == 1
 
   /** True if any bit is 1. */
-  def any: Boolean = bits.nonEmpty
+  val any: Boolean = bits != 0
 
   /** Returns the bitwise AND of two bit sequences. The length of the new sequence is the length of the longer sequence.
     *
@@ -39,23 +36,17 @@ final class BitSeq private(private val bits: BitSet, val length: Int) {
     * @tparam A the element type
     * @return the filtered sequence
     */
-  def filter[A](xs: Seq[A]): Seq[A] = (xs.view.zipWithIndex filter (x => bits(x._2)) map (_._1)).toSeq
+  def filter[A](xs: Seq[A]): Seq[A] = (xs.view.zipWithIndex filter (x => this (x._2)) map (_._1)).toSeq
 
-  /** Converts the bit sequence to an integer.
-    *
-    * @throws RuntimeException if the value is too large
-    */
-  def toInt: Int = bits.toBitMask match {
-    case Array(n) if n <= Int.MaxValue => n.toInt
-    case _ => error("Bit sequence is too large")
-  }
+  /** Converts the bit sequence to an integer. */
+  val toInt: Int = bits
 
   override def equals(obj: Any): Boolean = obj match {
     case other: BitSeq => other.bits == bits && other.length == length
     case _ => false
   }
 
-  override def hashCode: Int = bits.hashCode
+  override val hashCode: Int = bits.hashCode
 }
 
 /** Factories for bit sequences. */
@@ -65,8 +56,21 @@ object BitSeq {
     * @param booleans the boolean sequence
     * @return the bit sequence
     */
-  def apply(booleans: Boolean*): BitSeq =
-    new BitSeq(BitSet((booleans.view.zipWithIndex filter (_._1) map (_._2)).toSeq: _*), booleans.length)
+  def apply(booleans: Boolean*): BitSeq = new BitSeq(booleansToInt(booleans), booleans.length)
+
+  /** Converts the boolean sequence to an integer.
+    *
+    * @param booleans the boolean sequence
+    * @return the integer
+    */
+  private def booleansToInt(booleans: Seq[Boolean]): Int = {
+    @scala.annotation.tailrec
+    def toInt(index: Int, sum: Int): Int =
+      if (index >= booleans.length) sum
+      else toInt(index + 1, if (booleans(index)) sum + (1 << index) else sum)
+
+    toInt(0, 0)
+  }
 
   /** The empty bit sequence. */
   val zero: BitSeq = BitSeq()
